40af40c00c5a536ca8131c09cff81dd5024d5034
Alexander Brosi <alexanderbrosi@gmx.de>
2026-05-23 10:45:49 +0200

    Initial Commit

---
+++ V2xSnifferClient/V2xSnifferClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  V2xSnifferClientApp.swift
+//  V2xSnifferClient
+//
+//  Created by Alex on 23.05.26.
+//
+
+import SwiftUI
+
+@main
+struct V2xSnifferClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
